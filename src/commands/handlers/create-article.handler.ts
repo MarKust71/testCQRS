616b059c1c2595080f28bd7873945447d4e5f08c
MareK <markust71@gmail.com>
2020-03-01 11:23:01 +0100

    still articleRepository.create is not a function

--- src/commands/handlers/create-article.handler.ts
+++ src/commands/handlers/create-article.handler.ts
@@ -1,15 +1,25 @@
-import { ICommandHandler, CommandHandler, EventPublisher } from '@nestjs/cqrs';
+// import { ICommandHandler, CommandHandler, EventPublisher } from '@nestjs/cqrs';
+import { ICommandHandler, CommandHandler } from '@nestjs/cqrs';
 import { CreateArticleCommand } from '../implementations/create-article.command';
-import { Article, createArticle } from '../../article.entity';
+// import { Article, createArticle } from '../../article.entity';
+import { Article } from '../../article.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @CommandHandler(CreateArticleCommand)
 export class CreateArticleHandler
   implements ICommandHandler<CreateArticleCommand> {
-  constructor(private readonly publisher: EventPublisher) {}
+  constructor(
+    @InjectRepository(Article)
+    private readonly articleRepository: Repository<Article>, // private readonly publisher: EventPublisher,
+  ) {}
   async execute(command: CreateArticleCommand) {
-    let article: Article = createArticle(command.articleDto);
-    article = this.publisher.mergeObjectContext(article);
-    article.commit();
+    // let article: Article = createArticle(command.articleDto);
+    // const article: Article = createArticle(command.articleDto);
+    const article = await this.articleRepository.create(command.articleDto);
+    this.articleRepository.save(article);
+    // article = this.publisher.mergeObjectContext(article);
+    // article.commit();
     return article;
   }
 }
